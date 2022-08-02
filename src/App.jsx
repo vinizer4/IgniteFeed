a36e3feedc1b00cd39db9370c60a54ea56c45a87
vinizer4 <vinicius.ts.online@gmail.com>
2022-08-01 23:23:52 -0300

    Adicionando comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,43 @@ import styles from "./App.module.css";
 
 import './global.css';
 
+/*
+author: { avatar_url: "", name: "", role: ""}
+publishedAt: Date
+content: String
+*/
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/vinizer4.png',
+      name: 'Vinizer4',
+      role: 'Software Engineer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -14,17 +51,17 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-          author="Diego Fernandes" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facere dolorum eveniet, autem iste reiciendis! Est expedita inventore nesciunt soluta ipsam porro eius, quas dolores nihil enim temporibus non esse."
-          />
-          <Post 
-          author="Gabriel Buzzi"
-          content="Um novo post muito legal"
-          />
+          {posts.map(post => {
+           return (
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+           )
+          })}
         </main>
       </div>
     </div>
   )
 }
-
-
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,40 +1,62 @@
-import { Avatar } from "./Avatar"
-import { Comment } from "./Comment"
+import { format, formatDistanceToNow} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
+
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
+
 import styles from "./Post.module.css"
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+    const [comments, setComments] = useState([
+        'Post muito bacana, hein?!'
+    ])
+
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLL 'às' HH:mm'h'", {locale: ptBR}); 
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
+
+    function handleCreateNewComment() {
+        event.preventDefault()
+
+        setComments([...comments, newCommentText]);
+
+    }
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/vinizer4.png" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Vinicius Teixeira</strong>
-                        <span>JavaScript Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                {publishedDateRelativeToNow}
+                </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galera 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifolio. É um projeto que fiz na trilha Ignite React.js.</p>
-                <p>
-                    <a href="">👉{' '}jane.desing/doctorcare</a>
-                </p> 
-
-                <p>
-                    <a href="">#novoprojeto</a>{' '}
-                    <a href="">#ignite</a>{' '}
-                    <a href="">#rocketseat</a>{' '}
-                </p>
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p>{line.content}</p>
+                    } else if (line.type === 'link') {
+                        return <p><a href="#">{line.content}</a></p>
+                    }
+                })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
+                    name='comment'
                     placeholder="Deixe um comentário"
                 />
 
@@ -46,9 +68,9 @@ export function Post() {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment content={comment} />
+                })}
             </div>
         </article>
     )
