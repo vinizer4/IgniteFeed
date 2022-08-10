a28e4468566afacac5044159d00ef4ceb8481bb1
vinizer4 <vinicius.ts.online@gmail.com>
2022-08-10 00:23:53 -0300

    Add like function

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -30,7 +30,12 @@ export function Post({ author, publishedAt, content }) {
     };
     
     function handleNewCommentChange() {
+        event.target.setCustomValidity('')
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório!')
     }
 
     function deleteComment(comment) {
@@ -42,6 +47,8 @@ export function Post({ author, publishedAt, content }) {
 
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0
 
     return (
         <article className={styles.post}>
@@ -77,13 +84,16 @@ export function Post({ author, publishedAt, content }) {
                     placeholder="Deixe um comentário"
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type="submit" >Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
-
-                
+ 
             </form>
 
             <div className={styles.commentList}>
